ed3714147c5e5345ae5dd52c10ca4172f6cce1a0
=edouardkombo <=edouard.kombo@gmail.com>
2014-12-01 00:21:41 +0100

    prod payment

--- web/js/cart.js
+++ web/js/cart.js
@@ -180,8 +180,8 @@ shoppingCart.prototype.checkoutPayPal = function (parms, clearCart) {
 
     // build form
     var form = $('<form/></form>');
-    //form.attr("action", "https://www.paypal.com/cgi-bin/webscr");
-    form.attr("action", "https://www.sandbox.paypal.com/us/cgi-bin/webscr");
+    form.attr("action", "https://www.paypal.com/cgi-bin/webscr");
+    //form.attr("action", "https://www.sandbox.paypal.com/us/cgi-bin/webscr");
     form.attr("method", "POST");
     form.attr("style", "display:none;");
     this.addFormFields(form, data);
